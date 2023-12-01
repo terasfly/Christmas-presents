0fe911945de35814fe71b11916fedb40035a6a76
terasfly <84103159+terasfly@users.noreply.github.com>
2023-12-01 11:19:35 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,45 @@
-const matas = {
-    image1: 'images/wivter.jpg',
-    image2: 'images/wivter.jpg',
-    image3: 'images/snow.png'
-}
-
-const laura = {
-    image1: 'images/8.png'
-}
 const image1 = document.getElementById('image1')
 const image2 = document.getElementById('image2')
+const image3 = document.getElementById('image3')
+const image4 = document.getElementById('image4')
+
+const images = document.querySelectorAll('.image-class')
 
 
-image1.addEventListener('click', () => {
-    image1.src = matas.image1
-})
-image2.addEventListener('click', () => {
-    image2.src = matas.image2
-})
+
+
+const memberMatas = document.getElementById('member--matas')
+
+
+const matas = [
+    'images/matas/x.jpg',
+    'images/matas/xbo.jpg',
+    'images/matas/xb.jpg',
+    'images/face1.jpg'
+]
+
+images.forEach(allImages => {
+
+    memberMatas.addEventListener('click', () => {
+        allImages.addEventListener('click', changeImage)
+    })
+});
+
+function changeImage() {
+    const randomNumber = Math.floor(Math.random() * matas.length)
+    console.log(randomNumber)
+    this.src = matas[randomNumber];
+
+
+}
+
+
+// image1.addEventListener('click', () => {
+//     image1.src = matas.image1
+// })
+// image2.addEventListener('click', () => {
+//     image2.src = matas.image2
+// })
 
 
 // rightBtn.addEventListener('click', () => {
@@ -51,60 +74,58 @@ image2.addEventListener('click', () => {
 
 
 // auto play function
-function autoPlay() {
-
-}
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const snowflakes = document.querySelectorAll('.snowflake');
-
-    // Initial snowflake settings
-    snowflakes.forEach(snowflake => {
-        setSnowflakeInitialProperties(snowflake);
-    });
-
-    // Function to gradually increase speed
-    function increaseSpeedGradually() {
-        snowflakes.forEach(snowflake => {
-            let currentDuration = parseFloat(snowflake.style.animationDuration);
-            if (currentDuration > 2) { // Minimum duration limit (2s in this case)
-                snowflake.style.animationDuration = `${currentDuration - 0.1}s`; // Reduce duration gradually
-            }
-        });
-    }
-
-    // Call the function every 1 second
-    const increaseSpeedInterval = setInterval(increaseSpeedGradually, 1000);
-
-    // Optional: Clear interval after a certain time if needed
-    setTimeout(() => {
-        clearInterval(increaseSpeedInterval);
-    }, 15000); // Stop increasing speed after 10 seconds
-
-    function setSnowflakeInitialProperties(snowflake) {
-        snowflake.style.left = Math.random() * 100 + '%';
-        snowflake.style.opacity = Math.random();
-        snowflake.style.fontSize = Math.random() * (20 - 8) + 50 + 'px';
-        snowflake.style.animationDuration = 8 + Math.random() * 5 + 's'; // Initial falling duration
-        snowflake.style.animationDelay = Math.random() * 5 + 's';
-    }
-});
 
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    const snowflakesContainer = document.querySelector('.snowflakes');
-    const snowBottom = document.createElement('div');
-    snowBottom.classList.add('snow-bottom');
-    document.body.appendChild(snowBottom);
+// document.addEventListener('DOMContentLoaded', function() {
+//     const snowflakes = document.querySelectorAll('.snowflake');
 
-    setInterval(() => {
-        const maxHeight = 1000; // Maximum height of snow
-        const increase = 1; // Height increase per interval
-        const currentHeight = parseInt(getComputedStyle(snowBottom).height, 10);
-        if (currentHeight < maxHeight) {
-            snowBottom.style.height = `${currentHeight + increase}px`;
-        }
-    }, 1000); // Time interval in milliseconds
-});
+//     // Initial snowflake settings
+//     snowflakes.forEach(snowflake => {
+//         setSnowflakeInitialProperties(snowflake);
+//     });
+
+//     // Function to gradually increase speed
+//     function increaseSpeedGradually() {
+//         snowflakes.forEach(snowflake => {
+//             let currentDuration = parseFloat(snowflake.style.animationDuration);
+//             if (currentDuration > 2) { // Minimum duration limit (2s in this case)
+//                 snowflake.style.animationDuration = `${currentDuration - 0.1}s`; // Reduce duration gradually
+//             }
+//         });
+//     }
+
+//     // Call the function every 1 second
+//     const increaseSpeedInterval = setInterval(increaseSpeedGradually, 1000);
+
+//     // Optional: Clear interval after a certain time if needed
+//     setTimeout(() => {
+//         clearInterval(increaseSpeedInterval);
+//     }, 15000); // Stop increasing speed after 10 seconds
+
+//     function setSnowflakeInitialProperties(snowflake) {
+//         snowflake.style.left = Math.random() * 100 + '%';
+//         snowflake.style.opacity = Math.random();
+//         snowflake.style.fontSize = Math.random() * (20 - 8) + 50 + 'px';
+//         snowflake.style.animationDuration = 8 + Math.random() * 5 + 's'; // Initial falling duration
+//         snowflake.style.animationDelay = Math.random() * 5 + 's';
+//     }
+// });
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const snowflakesContainer = document.querySelector('.snowflakes');
+//     const snowBottom = document.createElement('div');
+//     snowBottom.classList.add('snow-bottom');
+//     document.body.appendChild(snowBottom);
+
+//     setInterval(() => {
+//         const maxHeight = 1000; // Maximum height of snow
+//         const increase = 1; // Height increase per interval
+//         const currentHeight = parseInt(getComputedStyle(snowBottom).height, 10);
+//         if (currentHeight < maxHeight) {
+//             snowBottom.style.height = `${currentHeight + increase}px`;
+//         }
+//     }, 1000); // Time interval in milliseconds
+// });
