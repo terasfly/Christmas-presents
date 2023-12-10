4f6606366dc13135613097c386edc1eaa7636e9a
terasfly <84103159+terasfly@users.noreply.github.com>
2023-12-10 12:22:51 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,202 +1,202 @@
-const image1 = document.getElementById('image1')
-const image2 = document.getElementById('image2')
-const image3 = document.getElementById('image3')
-const image4 = document.getElementById('image4')
+// Image Elements
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const image4 = document.getElementById('image4');
 
+// Flags to control image changes
+let canChangeImageMatas = false;
+let canChangeImageLaura = false;
+let canChangeImageEgle = false;
 
-let canChangeImageMatas = false
-let canChangeImageLaura = false
-let canChangeImageEgle = false
+// Image index counters for each member
+let currentIndexMatas = 0;
+let currentIndexEgle = 0;
+let currentIndexLaura = 0;
 
-const memberMatas = document.getElementById('member--matas')
+// Member Elements
+const memberMatas = document.getElementById('member--matas');
+const memberLaura = document.getElementById('member--laura');
+const memberEgle = document.getElementById('member--egle');
 
-
-
-const memberLaura = document.getElementById('member--laura')
-const memberEgle = document.getElementById('member--egle')
-
-const imagesLaura = document.querySelectorAll('.image-class')
+// Image class elements
+const imagesLaura = document.querySelectorAll('.image-class');
 const images = document.querySelectorAll('.image-class');
 const imagesEgle = document.querySelectorAll('.image-class');
-// laura setup 
 
-const laura = [
-
+// Image arrays for each member
+const lauraImages = [
     'images/matas/m&ms.png',
-    'images/face1.jpg',
-    'images/face1.jpg',
-    'images/face1.jpg'
-]
-const egle = [
-
     'images/matas/m&ms.png',
-    'images/face2.jpg',
-    'images/face2.jpg',
-    'images/face2.jpg'
-]
-memberLaura.addEventListener('click', () => {
-    canChangeImageLaura = true
+    'images/matas/m&ms.png',
+    'images/laura/astronaut.png',
 
-})
+];
+
+const egleImages = [
+    'images/matas/m&ms.png',
+    'images/egle/massage.png',
+    'images/egle/lupdazis.png',
+    'images/egle/coffe.png'
+];
+// member button active
+// Select all elements with the class 'member'
+const members = document.querySelectorAll('.member');
+
+// Function to remove 'clicked' class from all members
+function removeClickedClass() {
+    members.forEach(member => {
+        member.classList.remove('clicked');
+    });
+}
+
+// Add a click event listener to each member
+members.forEach(member => {
+    member.addEventListener('click', function() {
+        // Remove 'clicked' class from all members
+        removeClickedClass();
+
+        // Add 'clicked' class to the clicked member
+        this.classList.add('clicked');
+    });
+});
+
+
+
+// Egle setup
+// Enable image changing when Egle is clicked
 memberEgle.addEventListener('click', () => {
-    canChangeImageEgle = true
+    canChangeImageEgle = true;
+});
 
-})
-
+// Handle image clicks for Egle
 images.forEach(image => {
     image.addEventListener('click', () => {
         if (canChangeImageEgle) {
-            changeImageEgle.call(image)
-            canChangeImageEgle = false
-        }
-    })
-
-});
-
-function changeImageEgle() {
-    const randomNumber = Math.floor(Math.random() * egle.length);
-    this.src = egle[randomNumber];
-}
-
-
-imagesLaura.forEach(image => {
-    image.addEventListener('click', () => {
-        if (canChangeImageLaura) {
-            changeImageLaura.call(image)
-            canChangeImageLaura = false
-        }
-    })
-});
-
-
-function changeImageLaura() {
-    const randomNumber = Math.floor(Math.random() * laura.length);
-    this.src = laura[randomNumber];
-}
-
-
-const matas = [
-    'images/matas/m&ms.png',
-    'images/matas/netflix.png',
-    'images/matas/poundgift.png',
-    'images/matas/xbox.jpg'
-
-
-
-
-]
-
-
-
-memberMatas.addEventListener('click', () => {
-
-    canChangeImageMatas = true; // Nustatome flag į true paspaudus ant memberMatas
-});
-
-
-
-images.forEach(image => {
-    image.addEventListener('click', () => {
-        if (canChangeImageMatas) {
-            changeImage.call(image); // Iškviečiame changeImage su tinkama konteksto nuoroda
-            canChangeImageMatas = false; // Nustatome flag atgal į false
+            changeImageIndexEgle(image);
+            canChangeImageEgle = false;
         }
     });
 });
 
-function changeImage() {
-    const randomNumber = Math.floor(Math.random() * matas.length);
-    this.src = matas[randomNumber];
+// Function to change Egle's image
+function changeImageIndexEgle(imageElement) {
+    if (currentIndexEgle < egleImages.length) {
+        imageElement.src = egleImages[currentIndexEgle];
+        currentIndexEgle += 1;
+    } else {
+        currentIndexEgle = 0;
+        imageElement.src = egleImages[currentIndexEgle];
+    }
 }
 
+// Laura setup
+// Enable image changing when Laura is clicked
+memberLaura.addEventListener('click', () => {
+    canChangeImageLaura = true;
+});
 
+// Handle image clicks for Laura
+imagesLaura.forEach(image => {
+    image.addEventListener('click', () => {
+        if (canChangeImageLaura) {
+            changeImageIndexLaura(image);
+            canChangeImageLaura = false;
+        }
+    });
+});
 
-// image1.addEventListener('click', () => {
-//     image1.src = matas.image1
-// })
-// image2.addEventListener('click', () => {
-//     image2.src = matas.image2
-// })
+// Function to change Laura's image
+function changeImageIndexLaura(imageElement) {
+    if (currentIndexLaura < lauraImages.length) {
+        imageElement.src = lauraImages[currentIndexLaura];
+        currentIndexLaura += 1;
+    } else {
+        currentIndexLaura = 0;
+        imageElement.src = lauraImages[currentIndexLaura];
+    }
+}
 
+// Matas setup
+// Image array for Matas
+const matasImages = [
+    'images/matas/m&ms.png',
+    'images/matas/netflix.png',
+    'images/matas/poundgift.png',
+    'images/matas/charger.png'
+];
 
-// rightBtn.addEventListener('click', () => {
-//     scrollAmount = 0;
-//     let slideTimer = setInterval(() => {
-//         slider.scrollLeft += 10
-//             // after press button left slides move
-//         scrollAmount += 10
-//         if (scrollAmount >= 300) {
-//             window.clearInterval(slideTimer)
-//                 // if press button imediatly clear slidetimer
-//         }
-//     }, 25)
-// })
+// Enable image changing when Matas is clicked
+memberMatas.addEventListener('click', () => {
+    canChangeImageMatas = true;
+});
 
-// leftBtn.addEventListener('click', () => {
-//         scrollAmount = 0;
-//         let slideTimer = setInterval(() => {
-//             slider.scrollLeft -= 10
-//                 // after press button left slides move
-//             scrollAmount += 10
-//             if (scrollAmount >= 300) {
-//                 window.clearInterval(slideTimer)
-//                     // if press button imediatly clear slidetimer
-//             }
-//         }, 25)
-//     })
-//     // Slider width values
-// function sw() {
-//     alert()
-// }
+// Handle image clicks for Matas
+images.forEach(image => {
+    image.addEventListener('click', () => {
+        if (canChangeImageMatas) {
+            changeImageMatas(image);
+            canChangeImageMatas = false;
+        }
+    });
+});
 
+// Function to change Matas's image
+function changeImageMatas(imageElement) {
+    if (currentIndexMatas < matasImages.length) {
+        imageElement.src = matasImages[currentIndexMatas];
+        currentIndexMatas += 1;
+    } else {
+        currentIndexMatas = 0;
+        imageElement.src = matasImages[currentIndexMatas];
+    }
+}
 
-// auto play function
-
-
-
+// Snow animation setup
 document.addEventListener('DOMContentLoaded', function() {
     const snowflakes = document.querySelectorAll('.snowflake');
 
-    // Initial snowflake settings
+    // Set initial properties for each snowflake
     snowflakes.forEach(snowflake => {
         setSnowflakeInitialProperties(snowflake);
     });
 
-    // Function to gradually increase speed
+    // Gradually increase the speed of the snowflakes
     function increaseSpeedGradually() {
         snowflakes.forEach(snowflake => {
             let currentDuration = parseFloat(snowflake.style.animationDuration);
-            if (currentDuration > 2) { // Minimum duration limit (2s in this case)
-                snowflake.style.animationDuration = `${currentDuration - 0.1}s`; // Reduce duration gradually
+            if (currentDuration > 2) {
+                snowflake.style.animationDuration = `${currentDuration - 0.1}s`;
             }
         });
     }
 
-    // Call the function every 1 second
+    // Increase speed every 1 second
     const increaseSpeedInterval = setInterval(increaseSpeedGradually, 1000);
 
-    // Optional: Clear interval after a certain time if needed
+    // Stop increasing speed after 15 seconds
     setTimeout(() => {
         clearInterval(increaseSpeedInterval);
-    }, 15000); // Stop increasing speed after 10 seconds
+    }, 15000);
 
+    // Set initial properties for snowflakes
     function setSnowflakeInitialProperties(snowflake) {
-        snowflake.style.left = Math.random() * 100 + '%';
+        snowflake.style.left = `${Math.random() * 100}%`;
         snowflake.style.opacity = Math.random();
-        snowflake.style.fontSize = Math.random() * (20 - 8) + 50 + 'px';
-        snowflake.style.animationDuration = 8 + Math.random() * 5 + 's'; // Initial falling duration
-        snowflake.style.animationDelay = Math.random() * 5 + 's';
+        snowflake.style.fontSize = `${Math.random() * (20 - 8) + 50}px`;
+        snowflake.style.animationDuration = `${8 + Math.random() * 5}s`;
+        snowflake.style.animationDelay = `${Math.random() * 5}s`;
     }
 });
 
-
-
+// Accumulating snow effect
 document.addEventListener('DOMContentLoaded', function() {
     const snowflakesContainer = document.querySelector('.snowflakes');
     const snowBottom = document.createElement('div');
     snowBottom.classList.add('snow-bottom');
     document.body.appendChild(snowBottom);
 
+    // Gradually increase the height of the accumulated snow
     setInterval(() => {
         const maxHeight = 100; // Maximum height of snow
         const increase = 1; // Height increase per interval
@@ -204,5 +204,20 @@ document.addEventListener('DOMContentLoaded', function() {
         if (currentHeight < maxHeight) {
             snowBottom.style.height = `${currentHeight + increase}px`;
         }
-    }, 1000); // Time interval in milliseconds
+    }, 1000);
 });
+
+// JavaScript to handle the click event
+// document.addEventListener('DOMContentLoaded', function() {
+//     const members = document.querySelectorAll('.family-members .member');
+
+//     members.forEach(member => {
+//         member.addEventListener('click', function() {
+//             // Remove the active class from all members
+//             members.forEach(m => m.classList.remove('member-active'));
+
+//             // Add the active class to the clicked member
+//             this.classList.add('member-active');
+//         });
+//     });
+// });
